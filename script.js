efd710c70c2da6c730098d4e6cfc96d0a3883217
Piotr Fortuna <pitrekf@gmail.com>
2025-10-09 19:55:59 +0200

    Poprawiono okienko newsletter (naprawione zamykanie i zapamiętywanie stanu)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 🔹 Supabase
 const SUPABASE_URL = 'https://mkvpqnvlzdrujsqkdpmi.supabase.co';
-const SUPABASE_KEY = 'YOUR_SUPABASE_KEY';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rdnBxbnZsemRydWpzcWtkcG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDc4MDcsImV4cCI6MjA3NTQyMzgwN30.QuCU__UgvzofofS-T5Y-XzdLW7EakZZzh4DwQP4xAnA';
 let supabase;
 
 // 🔹 Dane i koszyk
